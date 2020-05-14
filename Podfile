49545ba8dde19cdee0484f90ec01522a5f33df27
tzuting98914 <bee87914@gmail.com>
2020-05-14 11:41:15 +0800

    login

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '9.0'
 
 target 'Project' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -8,6 +8,7 @@ target 'Project' do
   pod 'Firebase/Database'
   pod 'Firebase/Auth'
   pod 'Firebase/Storage'
+  pod 'Firebase/Firestore'
   # Pods for Project
 
   target 'ProjectTests' do
--- Project/SignupViewController.swift
+++ Project/SignupViewController.swift
@@ -7,24 +7,81 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var phoneNumberinput: UITextField!
+    @IBOutlet weak var emailinput: UITextField!
+    @IBOutlet weak var passwordinput: UITextField!
+    @IBOutlet weak var singup: UIButton!
+    @IBOutlet weak var errormessange: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func isPasswordValid(_ Password : String) -> Bool{
+        let Password = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Z](?=.*[0-9].{>8}$")
+        return Password.evaluate(with: Password)
     }
-    */
+    
+    
+    func validateFields() -> String? {
+        //check is empty
+        if phoneNumberinput.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailinput.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordinput.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        {return "Please input"}
+        
+        //check is password
+        let cleanedPassword = passwordinput.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if isPasswordValid(cleanedPassword) == false{
+            //password isn't secure
+            return "Your password is not secure.Please"
+        }
+        
+        return nil
+    }
 
+    func showerror(_ message:String){
+        errormessange.text = message
+        errormessange.alpha = 1
+    }
+    @IBAction func signupbtn(_ sender: Any) {
+        let error = validateFields()
+  
+        let email = emailinput.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let phonenumber = phoneNumberinput.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordinput.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        if error != nil {
+            showerror(error!)
+        }
+        else{
+            Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>) { (result, err) in
+                //check error
+                if err != nil{
+                    //an error
+                    self.showerror("error creating user")
+                }
+                else{
+                    //create successfully
+                    let db = Firestore.firestore()
+                    
+                    db.collection("user").addDocument(data: ["phonenumber":phonenumber, "email": email]){ (error) in
+                        if error != nil{
+                            self.showerror("error")
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+    }
 }
